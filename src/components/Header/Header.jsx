dae9ace0b86a268a3bb4ff6a29fd738126416dd6
Blackgolyb <andrejomelnickij@gmail.com>
2023-09-21 02:42:29 +0300

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return <header className={styles["header"]}></header>;
+};
+
+export default Header;
