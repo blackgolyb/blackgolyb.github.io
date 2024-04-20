e8902d9d92d5a5d464515e3b7624bda65696fa39
Blackgolyb <andrejomelnickij@gmail.com>
2024-04-20 18:46:16 +0300

    started refactoring in About section

--- src/components/apps/About/About.jsx
+++ src/components/apps/About/About.jsx
@@ -1,29 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { makeAutoInput } from "utils/autoInput";
+import React from "react";
+
+import ASCIIButton from "components/ASCII/ASCIIButton/ASCIIButton";
+
+import { Str } from "utils/autoInput";
+import AutoInputComponent from "components/AutoInputComponent/AutoInputComponent";
 
 import styles from "./About.module.css";
 
 const About = (props) => {
-    const [aboutText, setAboutText] = useState("");
-
     const text = "Test text for test";
 
-    useEffect(() => {
-        const autoInput = makeAutoInput();
-        autoInput(text, setAboutText);
+    const onAnimationFinish = () => {
         props.terminalRef?.current.exit();
-        console.log("test");
-    }, []);
+    };
+
+    const emulateCommand = (command) => {
+        props.terminalRef?.current.emulateCommand(command);
+    };
 
     return (
-        <div>
-            <img
-                src="https://avatars.githubusercontent.com/u/61083295?v=4"
-                alt="avatar"
-                className={styles["avatar"]}
-            />
-            About <span>{aboutText}</span>
-        </div>
+        <>
+            <AutoInputComponent callback={onAnimationFinish}>
+                <div>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/61083295?v=4"
+                        alt="avatar"
+                        className={styles["avatar"]}
+                    />
+                    <span>
+                        <Str>{text}</Str>
+                    </span>
+                </div>
+            </AutoInputComponent>
+            <ASCIIButton
+                onClick={(e) => {
+                    emulateCommand("experience");
+                }}
+                className={styles["next-btn"]}
+            >
+                <Str>Next</Str>
+            </ASCIIButton>
+        </>
     );
 };
 
