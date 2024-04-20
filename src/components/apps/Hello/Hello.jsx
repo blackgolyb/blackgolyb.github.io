bc672d9efae3e2f9239f5d4c8ba583817bd18baf
Blackgolyb <andrejomelnickij@gmail.com>
2024-04-20 15:05:32 +0300

    added step-by-step component rendering and use it in Hello section

--- src/components/apps/Hello/Hello.jsx
+++ src/components/apps/Hello/Hello.jsx
@@ -1,11 +1,82 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
+
+import { Str, useAutoInputComponent } from "utils/autoInput";
+
+import style from "./Hello.module.css";
 
 const Hello = (props) => {
+    const [text, setText] = useAutoInputComponent(30, [-20, 20]);
+
+    const t1 = `Hello! Welcome to my portfolio terminal.
+
+This is not your typical website - it's designed to resemble a command line interface, where you can navigate through my projects and learn more about me using commands.
+
+Here are a few basic commands to get you started:`;
+
+    const t2 = `Feel free to explore! If you ever need assistance, just type help or chose one of command in menu.
+
+Let's start from about section`;
+
+    const apps = [
+        {
+            name: "help",
+            description: "return a list of all available commands",
+        },
+        {
+            name: "projects",
+            description: "lists all the projects in my portfolio",
+        },
+        {
+            name: "about",
+            description: "provides information about me",
+        },
+        {
+            name: "contact",
+            description: "shows how you can reach out to me",
+        },
+    ];
+
+    const emulateCommand = (command) => {
+        props.terminalRef?.current.emulateCommand(command);
+    };
+
     useEffect(() => {
-        props.terminalRef?.current.exit();
+        setText(
+            <div>
+                <p>
+                    <Str>{t1}</Str>
+                </p>
+                <ul>
+                    {apps.map((app) => {
+                        return (
+                            <li>
+                                <button
+                                    className={style["command-btn"]}
+                                    onClick={() => {
+                                        emulateCommand(app.name);
+                                    }}
+                                >
+                                    <Str>{app.name}</Str>
+                                </button>
+                                <Str>{` - ${app.description}`}</Str>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <p>
+                    <Str>{t2}</Str>
+                </p>
+            </div>,
+            () => {
+                setTimeout(() => {
+                    emulateCommand("about");
+                }, 500);
+                props.terminalRef?.current.exit();
+            }
+        );
     }, []);
 
-    return <div>Hello</div>;
+    return <div className={style["hello"]}>{text}</div>;
 };
 
 export default Hello;
--- src/utils/autoInput.js
+++ src/utils/autoInput.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 import { getRndInteger } from "utils/utils";
 
-export function makeAutoInput (defaultInterval=150, randomRange=[-50, 100]) {
-    function autoInput(text, setFunction) {
+export function makeAutoInput(defaultInterval = 150, randomRange = [-50, 100]) {
+    function autoInput(text, setFunction, endCallback) {
         let i = 0;
 
         const doIteration = () => {
-            if (i > text.length) return;
+            if (i > text.length) {
+                if (endCallback !== undefined) endCallback();
+                return;
+            }
 
             setFunction(text.slice(0, i));
             i++;
 
-            const delay = getRndInteger(randomRange[0], randomRange[1]) + defaultInterval;
+            const delay =
+                getRndInteger(randomRange[0], randomRange[1]) + defaultInterval;
             setTimeout(doIteration, delay);
         };
 
@@ -19,3 +25,156 @@ export function makeAutoInput (defaultInterval=150, randomRange=[-50, 100]) {
 
     return autoInput;
 }
+
+export const Str = (props) => {
+    return props.children;
+};
+
+const isInstanceReact = (elem, baseType) => {
+    return typeof elem === "object" && "type" in elem && elem.type === baseType;
+};
+
+const isReactElemWithChildren = (elem) => {
+    return (
+        typeof elem === "object" && "props" in elem && "children" in elem.props
+    );
+};
+
+const treeToList = (component) => {
+    let res = [];
+
+    if (component instanceof Array) {
+        for (let i = 0; i < component.length; i++) {
+            res = [...res, ...treeToList(component[i])];
+        }
+    } else if (isInstanceReact(component, Str)) {
+        res.push(component);
+    } else if (
+        typeof component === "object" &&
+        "props" in component &&
+        "children" in component.props
+    ) {
+        res = [...res, component, ...treeToList(component.props.children)];
+    } else {
+        res.push(component);
+    }
+
+    return res;
+};
+
+const treeSliceToNode = (component, node, findConstructorCallback, current) => {
+    let res;
+    let progress = true;
+
+    if (current === undefined) {
+        current = component;
+    }
+
+    if (node === current) {
+        if (findConstructorCallback !== undefined) {
+            res = findConstructorCallback(current);
+        } else {
+            if (
+                !isInstanceReact(current, Str) &&
+                isReactElemWithChildren(current)
+            ) {
+                res = { ...current };
+                res.props = {};
+            } else {
+                res = current;
+            }
+        }
+
+        return [false, res];
+    } else if (current instanceof Array) {
+        res = [];
+        for (let i = 0; i < current.length; i++) {
+            const [tmpProgress, tmp] = treeSliceToNode(
+                component,
+                node,
+                findConstructorCallback,
+                current[i]
+            );
+            progress = tmpProgress;
+            res.push(tmp);
+            if (tmpProgress === false) {
+                break;
+            }
+        }
+    } else if (isReactElemWithChildren(current)) {
+        const [tmpProgress, tmp] = treeSliceToNode(
+            component,
+            node,
+            findConstructorCallback,
+            current.props.children
+        );
+        progress = tmpProgress;
+        res = { ...current };
+        res.props = { ...current.props };
+        res.props.children = tmp;
+    } else {
+        res = current;
+    }
+
+    return [progress, res];
+};
+
+export const useAutoInputComponent = (interval = 150, randomRange = [-50, 100]) => {
+    const [state, setState] = useState();
+    const [isRun, setIsRun] = useState(false);
+    const autoInput = makeAutoInput(interval, randomRange);
+
+    const start = (component, callback) => {
+        if (isRun) {
+            return;
+        }
+        setIsRun(true);
+
+        const nodesList = treeToList(component);
+
+        const doIteration = (i = 0) => {
+            if (i >= nodesList.length) {
+                setIsRun(false);
+                if (callback !== undefined) callback();
+                return;
+            }
+
+            const current = nodesList[i];
+
+            if (isInstanceReact(current, Str)) {
+                const slice = treeSliceToNode(component, current, (elem) => {
+                    const tmp = { ...elem };
+                    tmp.props = {};
+                    return tmp;
+                })[1];
+                setState(slice);
+
+                const text = current.props.children;
+                const setFunction = (newText) => {
+                    const updatedComponent = treeSliceToNode(
+                        component,
+                        current,
+                        (elem) => {
+                            const tmp = { ...elem };
+                            tmp.props = { children: newText };
+                            return tmp;
+                        }
+                    );
+
+                    setState(updatedComponent);
+                };
+                autoInput(text, setFunction, () => {
+                    doIteration(i + 1);
+                });
+            } else {
+                const slice = treeSliceToNode(component, current)[1];
+                setState(slice);
+                doIteration(i + 1);
+            }
+        };
+
+        doIteration();
+    };
+
+    return [state, start];
+};
