02be9dc4574fd15a5ebeaa4d6e747a45cfb5acdf
Blackgolyb <andrejomelnickij@gmail.com>
2024-08-28 00:43:38 +0300

    fix: fix bug with visibility in ascii components while animation going

--- src/components/ASCII/ASCIIUtils.jsx
+++ src/components/ASCII/ASCIIUtils.jsx
@@ -1,11 +1,10 @@
-import { ASCIIWrapper as RawASCIIWrapper } from "ascii-wrapper";
-import {
-	Scope,
-	AutoStr,
-	Callback,
-} from "src/components/AnimationFlow";
-import styles from "./ASCIIUtils.module.css";
 import { useCallback, useState } from "react";
+import { ASCIIWrapper as RawASCIIWrapper } from "ascii-wrapper";
+import cn from "clsx";
+
+import { Scope, AutoStr, Callback } from "src/components/AnimationFlow";
+
+import styles from "./ASCIIUtils.module.css";
 
 export const TopBorder = ({ priority = 3 }) => {
 	const { ASCIIBorders } = RawASCIIWrapper.useContext();
@@ -38,26 +37,39 @@ export const BottomBorder = ({ priority = 1 }) => {
 };
 
 export const ASCIIWrapper = ({ children, ...rest }) => {
-	const [isAnimating, setIsAnimating] = useState(true);
+	const [animating, setAnimating] = useState(0);
+
+	const setAnimRunning = useCallback(() => {
+		setAnimating(1);
+	}, []);
+
+	const setAnimFinish = useCallback(() => {
+		setAnimating(2);
+	}, []);
 
 	return (
-		<Scope>
-			<RawASCIIWrapper
-				border={
-					<>
-						<TopBorder />
-						<BottomBorder />
-					</>
-				}
-				bordersClassName={isAnimating ? styles["borders-animated"] : ""}
-				{...rest}
-			>
-				{children}
-			</RawASCIIWrapper>
-			<Callback
-				localePriority={-1}
-				callback={useCallback(() => setIsAnimating(false), [])}
-			/>
-		</Scope>
+		<>
+			<Scope>
+				<Callback localePriority={1000} callback={setAnimRunning} />
+				<div
+					className={cn({ [styles["before-animating"]]: animating === 0 })}
+				/>
+				<RawASCIIWrapper
+					border={
+						<>
+							<TopBorder />
+							<BottomBorder />
+						</>
+					}
+					bordersClassName={cn({
+						[styles["borders-animated"]]: animating !== 2,
+					})}
+					{...rest}
+				>
+					{children}
+				</RawASCIIWrapper>
+				<Callback localePriority={-1} callback={setAnimFinish} />
+			</Scope>
+		</>
 	);
 };
