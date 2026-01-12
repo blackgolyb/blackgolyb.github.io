8f53e3a94d4521c1bfc562a1a3d10191a6ae0afa
Blackgolyb <andrejomelnickij@gmail.com>
2026-01-12 20:58:52 +0200

    faat: add TypeCommand as base class for auto type commands

---
+++ src/commands/TypeCommand.ts
@@ -0,0 +1,37 @@
+import { BaseProcess } from "../process/BaseProcess";
+import { ProcessContext } from "../process/IProcess";
+import { renderTemplate } from "../utils/template";
+import { delay } from "../utils/textAnimations";
+
+export class TypeCommand extends BaseProcess {
+  static name = "base_type";
+  delayMs: number = 15;
+  text: string = "";
+  isSkipped: boolean = false;
+
+  protected async run(context: ProcessContext): Promise<void> {
+    const stdout = context.io.stdout;
+    for (const char of await this.renderText()) {
+      stdout.write(char);
+      if (!this.isSkipped) {
+        await delay(this.delayMs);
+      }
+    }
+  }
+
+  protected async getTextContext(): Promise<Record<string, any>> {
+    return {};
+  }
+
+  protected async renderText(): Promise<string> {
+    const text = renderTemplate(this.text, await this.getTextContext());
+    return text.replace(/\n/g, "\r\n");
+  }
+
+  onInput(data: string) {
+    console.log("Input received in TypeCommand:", JSON.stringify(data));
+    if (data === " " || data === "\r") {
+      this.isSkipped = true;
+    }
+  }
+}
